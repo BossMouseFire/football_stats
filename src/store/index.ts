ba44875758082f02eaa943e161fe6716303a55a0
Данил <abanin-02@mail.ru>
2021-09-07 17:35:21 +0400

    delete REDUX DEVTOOLS

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,5 +3,5 @@ import thunk from "redux-thunk"
 import {rootReducer} from "./reducers";
 
 // @ts-ignore
-const composeEnhancers = window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] as typeof compose || compose;
-export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+// const composeEnhancers = window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] as typeof compose || compose;
+export const store = createStore(rootReducer, applyMiddleware(thunk));
