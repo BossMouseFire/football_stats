c613839df4f1f38ada6e8756df3aed4b6e80a1f3
Данил <abanin-02@mail.ru>
2021-09-07 13:01:09 +0400

    deploy #3

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,21 @@ import React from 'react';
 import PageLeagues from './components/pageLeagues/pageLeagues'
 import PageLeague from "./components/pageLeague/pageLeague";
 import PageTeam from "./components/pageTeam/pageTeam";
-import {Switch, Route, Redirect, BrowserRouter} from "react-router-dom";
+import {Switch, Route, Redirect, HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./store";
 
 const App:React.FC = () => {
   return(
       <Provider store={store}>
-          <BrowserRouter>
+          <HashRouter basename={process.env.PUBLIC_URL}>
               <Switch>
                   <Route exact path={"/"} component={PageLeagues}/>
                   <Route path={"/league"} component={PageLeague}/>
                   <Route path={"/team"} component={PageTeam}/>
                   <Redirect to={"/"}/>
               </Switch>
-          </BrowserRouter>
+          </HashRouter>
       </Provider>
   )
 }
